565d6b8e0381310cf098219aca3821a35800c8ef
unknown <RLangton@DEGW01RLANGTON.degdigital.com>
2013-04-28 18:59:24 -0500

    added userregistrations join table for sessions

--- src/CodeSummit/Data/CodeSummitContext.cs
+++ src/CodeSummit/Data/CodeSummitContext.cs
@@ -12,5 +12,17 @@ namespace CodeSummit.Data
 
         public DbSet<UserRegistrationDbModel> UserRegistrations { get; set; }
         public DbSet<Session> Sessions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserRegistrationDbModel>().HasMany(x => x.Sessions).WithMany(x => x.UserRegistrations).Map(m
+                                                                                                                           =>
+                {
+                    m.ToTable("UserRegistrations_Sessions");
+                    m.MapLeftKey("UserRegistrationId");
+                    m.MapRightKey("SessionId");
+                });
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
