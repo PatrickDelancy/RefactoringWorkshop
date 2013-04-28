3207b0b3fcd9aaf61ff249f885f1053cb85b6300
unknown <RLangton@DEGW01RLANGTON.degdigital.com>
2013-04-28 18:04:53 -0500

    adding code first migrations

--- src/CodeSummit/Data/UserRegistrationDbModel.cs
+++ src/CodeSummit/Data/UserRegistrationDbModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using CodeSummit.Models;
 
 namespace CodeSummit.Data
 {
+    [Table("UserRegistrations")]
     public class UserRegistrationDbModel
     {
         public int Id { get; set; }
